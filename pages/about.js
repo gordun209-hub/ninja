26313d9e491416aec6fc3e8d398d5b5f7f15d3c6
gordun209 <ali-han80@hotmail.com>
2022-03-06 19:57:15 +0300

    styled

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,27 @@
-const about = () => {
+const About = () => {
   return (
     <div>
-      about
-      <p>qweo[dkqwekd</p>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+        reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+        laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+        sequi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+        reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+        laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+        sequi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+        reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+        laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+        sequi!
+      </p>
     </div>
   )
 }
 
-export default about
+export default About
